2d331978f31e57e100b1e6bf19624a950e2b9207
holyshitcode <164037052+holyshitcode@users.noreply.github.com>
2024-03-20 19:37:35 +0900

    Add files via upload

---
+++ bmath.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int A = 10, B = 20;
+	printf("%d\n", A + B);
+	printf("%d\n", A - B );
+	printf("%d\n", A * B );
+	printf("%d\n", A / B );
+	printf("%d\n", A % B);
+
+}

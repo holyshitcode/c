184226e8f70195bcd6ab337c63b213ef572be369
holyshitcode <164037052+holyshitcode@users.noreply.github.com>
2024-03-20 20:06:00 +0900

    Add files via upload

---
+++ west.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int fire, west;
+	scanf("%d", &fire);
+	west = fire - 543;
+	printf("%d", west);
+	return 0;
+	
+}

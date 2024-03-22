acf2e54aa3aa98a3bc7797896cc5502845ec2a2f
holyshitcode <164037052+holyshitcode@users.noreply.github.com>
2024-03-22 19:29:34 +0900

    Update 4x4.c
    work well now

--- 4x4.c
+++ 4x4.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
-rotate(int (*square))
-{
-    
-}
+#define _CRT_SECURE_NO_WARNINGS
+void Rotate(int(*square)[4]);
+void Showarr(int(*square)[4]);
 
 int main(void)
 {
@@ -13,13 +11,51 @@ int main(void)
         {9,10,11,12},
         {13,14,15,16}
     };
-    
-    for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
-            printf("%3d",square[i][j]);
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%3d", square[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        Rotate(square);
+        Showarr(square);
+    }
     return 0;
+
+}
+void Showarr(int(*square)[4])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++) {
+            printf(" %d", square[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+}
+void Rotate(int(*square)[4])
+{
+    int Temp[4][4];
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Temp[j][3 - i] = square[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            square[i][j] = Temp[i][j];
+        }
+    }
 
 }
